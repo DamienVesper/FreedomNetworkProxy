f8a14b5ecdb984f1409d911d79953d3f0cb54b34
Damien Vesper <ldamienvesper@gmail.com>
2022-01-30 14:18:16 -0600

    add ssl to conf

--- config/config.ts
+++ config/config.ts
@@ -1,6 +1,13 @@
+import * as path from 'path';
+
 const config = {
     port: 8443,
+
     ssl: false,
+    sslParams: {
+        key: path.resolve(__dirname, `../certs/default.key`),
+        cert: path.resolve(__dirname, `../certs/default.cert`)
+    },
 
     title: `FreedomNetworkProxy`,
     prefix: `/astrox/`,
