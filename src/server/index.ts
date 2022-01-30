80b05ebd975f83c1bd6ba07d479d2223bc1d2713
Damien Vesper <ldamienvesper@gmail.com>
2022-01-30 14:20:16 -0600

    do in script

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,5 +2,9 @@ import config from '../../config/config';
 
 import server from './modules/server';
 
-// Listen on specified port.
-server.listen(config.port);
+const main = async (): Promise<void> => {
+    // Listen on specified port.
+    server.listen(config.port);
+};
+
+void main();
