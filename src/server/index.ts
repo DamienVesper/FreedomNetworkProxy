9b0aee19c93ed90b731e6f61a264a08375ecf52e
Damien Vesper <ldamienvesper@gmail.com>
2022-01-30 14:19:25 -0600

    server initialization in a nutshell

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,1 @@
-import config from '../../config/config';
-import corrosion from 'corrosion';
-
-import * as path from 'path';
-import * as fs from 'fs';
-
-import * as http from 'http';
-import * as https from 'https';
-
+import './modules/server';
