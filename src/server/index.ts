57776f43a9d5088f5186f6f821b981c585b911f4
Damien Vesper <ldamienvesper@gmail.com>
2022-01-30 14:31:58 -0600

    server is a listening

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,9 +2,17 @@ import config from '../../config/config';
 
 import server from './modules/server';
 
+import log from './utils/log';
+import { logSplash, logHeader } from './utils/logExtra';
+
 const main = async (): Promise<void> => {
+    logSplash();
+
     // Listen on specified port.
-    server.listen(config.port);
+    server.listen(config.port, () => {
+        log(`green`, `Server is listening on port ${config.port}.`);
+        logHeader();
+    });
 };
 
 void main();
