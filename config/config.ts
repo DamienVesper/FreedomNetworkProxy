dd6c9eddb72fc8be54a94f90c9c5fa06bac5b710
Damien Vesper <ldamienvesper@gmail.com>
2022-01-30 14:00:25 -0600

    set config

--- config/config.ts
+++ config/config.ts
@@ -1,5 +1,12 @@
 const config = {
     port: 8443,
+    ssl: false,
+
+    title: `FreedomNetworkProxy`,
+    prefix: `/astrox/`,
+    codec: `xor`,
+    blacklist: [`accounts.google.com`],
+    smokeprefix: `/astrox2/`
 };
 
 export default config;
